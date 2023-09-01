e44ca799632dbcf938bf9dbb70b94eadff719935
atefeh-syf <atefe.s.77218@gmail.com>
2023-09-01 16:04:31 +0330

    channel example

--- 15-Concurrency/10-(Un)bufferedChannels/examples/unbufferedExamples.go
+++ 15-Concurrency/10-(Un)bufferedChannels/examples/unbufferedExamples.go
@@ -1,41 +1,56 @@
 package examples
 
 import (
-	"fmt"
 	"io"
 	"net/http"
 	"strconv"
 	"time"
 )
 
-func Emaple1() {
+func UnbufferedExample() {
+
+	now := time.Now()
+	// buffered channel unbuffered channel
+	// send only, receive only, send and receive
+	// sender := make(chan<- string)
+	// receiver := make(<-chan string)
 	content := make(chan string)
 	for i := 0; i < 100; i++ {
-		go GetHttpRequest(content, i+1)
+		go GetHttpRequestUnbuffered(content, i+1)
 	}
 
 	for i := 0; i < 100; i++ {
 		response := <-content
 		println(response)
 	}
+
+	println("Time: ", time.Since(now).Milliseconds())
 }
 
-func GetHttpRequest(content chan string, index int) {
+func GetHttpRequestUnbuffered(content chan<- string, index int) {
 	response, err := http.Get("https://jsonplaceholder.typicode.com/todos/" + strconv.Itoa(index))
 
 	if err != nil {
 		panic(err)
 	}
 
+	defer response.Body.Close()
+
 	responseBody, err := io.ReadAll(response.Body)
+
 	if err != nil {
 		panic(err)
 	}
-	PrintLnWithTime("Before set content")
+	PrintlnWithTime("Before set content")
 	content <- string(responseBody)
-	PrintLnWithTime("After set content")
+
+	PrintlnWithTime("After set content")
 }
 
-func PrintLnWithTime(args ...any) {
-	fmt.Printf("Time: %s, %v\n", time.Now().Format(time.RFC3339Nano), args)
+
+type Person struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	City      string `json:"city"`
 }
--- 15-Concurrency/10-(Un)bufferedChannels/main.go
+++ 15-Concurrency/10-(Un)bufferedChannels/main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"Unbuffered/examples"
-	"time"
 )
 
 func main() {
-	examples.Emaple1()
-}
-
-func IntroExample() {
-	numChannel := make(chan int)
-	go func() {
-		time.Sleep(time.Second * 3)
-		numChannel <- 1
-	}()
-	println("waiting for numChannel", time.Now().String())
-	receivedNum := <-numChannel
-	println(receivedNum)
+	examples.UnbufferedExample()
 }
