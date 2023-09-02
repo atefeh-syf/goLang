97c2bb5c94d9d989eea6fae49ef33010a4a54071
atefeh-syf <atefe.s.77218@gmail.com>
2023-09-02 14:07:57 +0330

    http mux

--- 17-Http/02-Server/main.go
+++ 17-Http/02-Server/main.go
@@ -2,6 +2,18 @@ package main
 
 import "http-examples/examples"
 
+// HTTP Server
+// Routing and handlers
+// HTTP request segments // url, method, path, query, params, headers, body
+// HTTP response segments , statusCode, body, headers
+// Context
+// Mini Project => Simple CRUD store without database
+
 func main() {
-	examples.CreateServer()
+	// 02- Create server
+	//examples.CreateServer()
+
+	// 03- Create server and servemux
+	examples.CreateServerWithMux()
+	//examples.CreateServerWithCustomHandler()
 }
