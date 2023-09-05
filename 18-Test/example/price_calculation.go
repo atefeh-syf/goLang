ff01d9b7a8fc30831f85b356fb42d231236d122e
atefeh-syf <atefe.s.77218@gmail.com>
2023-09-05 17:25:51 +0330

    fuzz test

--- 18-Test/example/price_calculation.go
+++ 18-Test/example/price_calculation.go
@@ -9,8 +9,8 @@ func CalculateRoomPrice(roomType string, nights int, personCount int) (finalPric
 		price = nights * 220000 * personCount
 	case "suite":
 		price = nights * 350000 * personCount
-	default:
-		panic("room type not supported")
+		// default:
+		// 	panic("room type not supported")
 	}
 	tax := float64(price) * 0.09
 	finalPrice = price + int(tax)
--- 18-Test/example/price_calculation_test.go
+++ 18-Test/example/price_calculation_test.go
@@ -4,6 +4,19 @@ import (
 	"testing"
 )
 
+type testCase struct {
+	RoomType   string
+	Nights     int
+	Persons    int
+	FinalPrice int
+}
+
+var testCases = []testCase{
+	{"standard", 2, 2, 610400},
+	{"double", 2, 1, 479600},
+	{"suite", 2, 2, 1526000},
+}
+
 func TestCalculateRoomPrice(t *testing.T) {
 	// Arrange
 	expectedRoomPrice := 610400
@@ -17,4 +30,20 @@ func TestCalculateRoomPrice(t *testing.T) {
 	if actualRoomPrice != expectedRoomPrice {
 		t.Errorf("Expected %v, got %v", expectedRoomPrice, actualRoomPrice)
 	}
+}
+
+func TestCalculateRoomPrice1(t *testing.T) {
+
+	for _, item := range testCases {
+		got := CalculateRoomPrice(item.RoomType, item.Nights, item.Persons)
+		if got != item.FinalPrice {
+			t.Errorf("Expected %v, got %v", item.FinalPrice, got)
+		}
+	}
+}
+
+func FuzzCalculateRoomPrice1(f *testing.F) {
+	f.Fuzz(func(t *testing.T, roomType string, nights int, personCount int) {
+		CalculateRoomPrice(roomType, nights, personCount)
+	})
 }
